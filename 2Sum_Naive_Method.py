b9a765f17beb4215bdeeb086a3c5302a4d02a469
HumanKing991 <humanplanet4082443@gmail.com>
2020-06-18 19:03:47 +0530

    First commit

--- 2Sum_Naive_Method.py
+++ 2Sum_Naive_Method.py
@@ -1,13 +1,11 @@
 def two_sum(lis, target):
     
-    for i in lis:
-        for j in range(1, len(lis)):
-            if i + lis[j] == target:
-                return [lis.index(i),j]
+    for i in range(len(lis)):
+        for j in range(i+1, len(lis)):
+            if lis[i]+ lis[j] == target:
+                return [i,j]
     return "Target Sum not found "
 
 target = int(input())
 lis = [int(x) for x in input().split()]
 print(two_sum(lis, target))
-                
-            
