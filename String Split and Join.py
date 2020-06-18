771f1e179b9ece765bccb2a353f1b56cfc1ebfa1
HumanKing991 <humanplanet4082443@gmail.com>
2020-06-18 13:35:16 +0530

    First commit

---
+++ String Split and Join.py
@@ -0,0 +1,25 @@
+def split_and_join(line):
+    # write your code here
+    x = line.split()
+    return '-'.join(x)
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
+
+'''Task
+You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+
+Input Format
+The first line contains a string consisting of space separated words.
+
+Output Format
+Print the formatted string as explained above.
+
+Sample Input
+
+this is a string   
+Sample Output
+
+this-is-a-string'''
