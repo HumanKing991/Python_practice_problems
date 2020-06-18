d2481de5e112d4926bdd8f543cdd0e142b3841eb
HumanKing991 <humanplanet4082443@gmail.com>
2020-06-18 14:12:19 +0530

    First commit

---
+++ Find a string.py
@@ -0,0 +1,21 @@
+def count_substring(string, sub_string):
+   
+    
+    
+    return sum(1 for i in range(len(string))  
+         if string.startswith(sub_string,i))
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
+
+'''
+Sample Input
+
+ABCDCDC
+CDC
+Sample Output
+
+2'''
